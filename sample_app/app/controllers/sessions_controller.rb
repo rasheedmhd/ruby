9a41626949f91bfce09d4ed6af21673f757763f6
Virux <starletgh@gmail.com>
2023-10-23 22:10:23 +0000

    finding and authenticating users

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(password: params[:session][:password])
+      # log in the login and redirect to the user's show page
+      else
+        # flash[:failed] = "User log in failed!"
+        render :new
+      end
+  end
+
+  def destroy
+  end
+
 end
