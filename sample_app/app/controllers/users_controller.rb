0c9cd56da82c6450facf782dd60c776d4c621d53
Virux <starletgh@gmail.com>
2023-10-27 17:05:41 +0000

    Completed user edit, update, index, and destroy actions

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :authorized_user, only: [:edit, :update]
+  before_action :logged_in_user,    only: [:index, :edit, :update, :destroy]
+  before_action :authorized_user,   only: [:edit, :update]
+  before_action :only_admin,        only: :destroy
+
+  def index
+      @users = User.paginate(page: params[:page])
+  end
 
   def new
       @user = User.new
@@ -51,6 +56,12 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
       # flash[:danger] = "Permission Denied"
+  end
+
+  def destroy
+      User.find(params[:id]).destroy
+      flash[:success] = "User deleted"
+      redirect_to users_url
   end
 
   private
