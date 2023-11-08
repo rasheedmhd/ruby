09919591989e0ce3ddcd9a60f75a1c1f5c2b4033
Virux <starletgh@gmail.com>
2023-11-08 11:00:23 +0000

    fixed delete links not working

--- sample_app/app/javascript/application.js
+++ sample_app/app/javascript/application.js
@@ -1,11 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
 
-// Dropdown still not working
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-import "bootstrap"
+
+// rails importmap:install $ rails turbo:install stimulus:install
+// Run turbo:install:redis to switch on Redis and use it in development for turbo streams
