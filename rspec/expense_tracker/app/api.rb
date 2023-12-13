234f4342fadd643670d73963a96f0235750e5678
rasheedmhd <starletgh@gmail.com>
2023-12-13 17:54:40 +0000

    sinatra

---
+++ rspec/expense_tracker/app/api.rb
@@ -0,0 +1,10 @@
+require 'sinatra/base'
+require 'json'
+
+
+module ExpenseTracker
+    class API < Sinatra::Base
+        post '/expenses' do
+        end
+    end
+end
---
+++ rspec/expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -0,0 +1,28 @@
+require 'rack/test'
+require 'json'
+
+require_relative '../../app/api'
+
+
+module ExpenseTracker
+    # frozen_string_literal: true
+    
+    RSpec.describe 'Expense Tracker API' do
+        include Rack::Test::Methods
+
+        def app
+            ExpenseTracker::API.new
+        end
+
+        it "records submitted expenses" do
+            coffee = {
+                'payee' => 'Starbucks',
+                'amount' => '5.75',
+                'date' => '2017-06-10',
+            }
+            
+            post '/expenses', JSON.generate(coffee)
+            expect(last_response.status).to eq(200)
+        end
+    end
+end
